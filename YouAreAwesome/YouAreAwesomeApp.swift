c14a306fd3088743feb1c74a587eb8e612b4736d
Richard Gagg <r.gagg@outlook.com>
2026-01-02 10:56:08 +1100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Richard Gagg on 2/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
