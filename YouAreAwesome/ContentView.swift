53bc96462e43318b51d3e821a69b4d8c6c3ec509
Richard Gagg <r.gagg@outlook.com>
2026-01-17 17:42:45 +1100

    CH 1.12. Updated button message and image randomisation with repeat while loop to eliminate repeated messages and images.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
   
   @State private var messageString: String = ""
   @State private var imageName: String = ""
+  @State private var lastMessageNumber = -1 //Will never test true in button
+  @State private var lastImageNumber = -1 //Will never test true in button
   
   @State private var messages: [String] = [
     "You Are Awesome!",
@@ -47,9 +49,20 @@ struct ContentView: View {
       
       Button("Show Message") {
         
-        messageString = (messages[Int.random(in: 0..<messages.count)])
+        var messageNumber: Int = Int.random(in: 0..<messages.count)
+        var imageNumber: Int = Int.random(in: 0...9)
         
-        imageName = "image\(Int.random(in: 0...9))"
+        repeat {
+          messageNumber = Int.random(in: 0..<messages.count)
+        } while messageNumber == lastMessageNumber
+        lastMessageNumber = messageNumber
+        messageString = (messages[messageNumber])
+
+        repeat {
+          imageNumber = Int.random(in: 0...9)
+        } while imageNumber == lastImageNumber
+        lastImageNumber = imageNumber
+        imageName = "image\(imageNumber)"
         
       }
       .buttonStyle(.glassProminent)
